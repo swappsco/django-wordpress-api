ece628895b3323aa45d99466a220286be5c38a2e
Kalle Westerling <7298727+kallewesterling@users.noreply.github.com>
2021-04-23 11:33:11 -0400

    Resolves #11
    Correcting the `example` app using (your documentation)[https://django-wordpress-api.readthedocs.io/en/latest/integration.html] which details that the `include` here should be for `wordpress_api.urls` (and not ~~`wordpress.urls`~~). See report in issue #11.

--- example/example/urls.py
+++ example/example/urls.py
@@ -19,5 +19,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'', include('wordpress.urls', namespace='wordpress')),
+    url(r'', include('wordpress_api.urls', namespace='wordpress')),
 ]
